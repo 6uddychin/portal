72f3927a9d63edaffa68d90570c25246bdc75d15
6uddychin <75438409+6uddychin@users.noreply.github.com>
2024-06-10 13:43:40 -0700

    initial commit

---
+++ src/ui-components/work_order.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import EnhancedTable from './DataTable';
+import Navbar from './ui-components/Navbar'; 
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import KnowledgeArticles from './ui-components/knowledgeArticles';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [messagesData, setMessagesData] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/vendor_portal_data.json')
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      });
+
+    fetch('/data/messages.json')
+      .then(response => response.json())
+      .then(data => {
+        setMessagesData(data);
+      });
+  }, []);
+
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <Router>
+          <div className="App">
+            <Navbar />
+            <h1>{user.username}!</h1>
+            <EnhancedTable data={data} />
+      <div style={{ borderTop: '1px solid #ccc', padding: '20px 0' }}></div>
+      <Routes>
+
+      <Route path="/KnowledgeArticles" element={<KnowledgeArticles />} />
+      </Routes>
+
+
+            <button onClick={signOut}>Sign out</button>
+          </div>
+        </Router>
+      )}
+    </Authenticator>
+  );
+}
+
+export default App;
+
+/*
+
+            <Routes>
+              <Route path="/" element={<EnhancedTable data={data} />} />
+              <Route path="/messages" element={<MessagesTable data={messagesData} />} />
+              <Route path="/knowledge-center" element={<KnowledgeArticles />} />
+            </Routes>
+            */
